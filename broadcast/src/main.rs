298d4a0dcdf77d2919dcd5bfc5abee0495c55b49
Simon Farre <simon.farre.cx@gmail.com>
2023-04-17 20:18:47 +0200

    Added data types for message enum variant

--- broadcast/src/main.rs
+++ broadcast/src/main.rs
@@ -1,16 +1,56 @@
-use virvelvind::{
-    requests::{Initialize, MaelstromRequest},
-    response::{MaelstromResponse, ResponseBody},
-    Deserialize, Node, Serialize,
-};
+use std::collections::{HashSet, HashMap};
+use virvelvind as vv;
+use vv::{requests::Initialize, Deserialize, Node, Serialize};
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RPCRead {
+  messages: HashSet<usize>
+}
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+pub struct Topology {
+  topology: HashMap<vv::NetworkEntityId, Vec<vv::NetworkEntityId>>
+}
+
+impl Topology {
+  pub fn neighborhood_of(&self, node_id: &vv::NetworkEntityId) -> Vec<vv::NetworkEntityId> {
+    self.topology.get(node_id).unwrap().clone()
+  }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "type", rename_all = "snake_case")] // make enum "internally tagged"
-pub enum BroadcastServiceDefinition {}
+/// https://fly.io/dist-sys/3a/ defines the broadcast service
+pub enum BroadcastServiceDefinition {
+    Broadcast { message: usize },
+    BroadcastOk,
+
+    Read,
+    ReadOk(RPCRead),
+
+    Topology(Topology),
+    TopologyOk,
+
+    Gossip { news: HashSet<usize> }
+}
 
 #[derive(Default)]
 pub struct BroadcastServiceNode {
     init: Initialize,
+    gossiped: HashSet<usize>,
+    news: HashSet<usize>,
+    neighbors: Topology
+}
+
+impl BroadcastServiceNode {
+  pub fn gossip_news(&mut self) -> HashSet<usize> {
+    let news_to_gossip_about: HashSet<usize> = self.gossiped.difference(&self.news).map(|x| *x).collect();
+    for item in &news_to_gossip_about {
+      self.gossiped.insert(*item);
+    }
+    news_to_gossip_about
+  }
 }
 
 impl Node<BroadcastServiceDefinition> for BroadcastServiceNode {
